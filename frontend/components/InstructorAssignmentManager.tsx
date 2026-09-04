1a2d451317f3a4e91d2f3223a505455f56716a9a
jonathanrhoffman81 <jonathanrhoffman81@gmail.com>
2026-09-04 17:01:19 -0400

    Filter instructor - 1

--- frontend/components/InstructorAssignmentManager.tsx
+++ frontend/components/InstructorAssignmentManager.tsx
@@ -1489,7 +1489,7 @@ export default function InstructorAssignmentManager({
                                                                     options={[
                                                                         { value: '', label: 'Add instructor…', disabled: true },
                                                                         ...sortedInstructors
-                                                                            .filter((instructor) => !groupInstructors.some((item) => item.id === instructor.person_id) && instructor.is_active == 'true')
+                                                                            .filter((instructor) => (!groupInstructors.some((item) => item.id === instructor.person_id) && instructor.is_active == 'true'))
                                                                             .map((instructor) => ({
                                                                                 value: instructor.person_id,
                                                                                 label: formatName(instructor.first_name, instructor.last_name),
